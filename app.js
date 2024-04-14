824ab3bf94f9d8b45ed6fd0ac5a2025498263d9e
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-04-14 10:16:36 +0530

    ServerPort Configured!

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const app = express();
 const auth = require("./routes/auth");
 const connectDB = require("./config/db");
+const PORT = 8000 || process.env.PORT;
 connectDB();
+
 app.use(express.json());
 app.get("/", (req, res) => {
-  res.send("Hey This is Wokring");
+  res.send("Server is Running");
 });
 app.use("/api/auth", auth);
-app.listen(8000, () => {
-  console.log("Server is Running!");
+app.listen(PORT, () => {
+  console.log(`Server is established at PORT - ${PORT}`);
 });
