c3d628de68a6ef71681d8237f3e43fa60f885324
Pratham Saxena <er.prathamsaxena@gmail.com>
2024-04-14 11:56:04 +0530

    Added Delete API

--- routes/todo_list.js
+++ routes/todo_list.js
@@ -14,12 +14,27 @@ router.post("/todo", async (req, res) => {
         user: existingUser,
       });
       list.save().then(() => res.status(200).json({ message: "TODO Saved" }));
-      // console.log(existingUser);
       existingUser.todo_list.push(list);
       existingUser.save();
     }
   } catch (err) {
     res.status(400).send({ message: "Failed to save todo: " + err.message });
+  }
+});
+
+router.delete("/todo", async (req, res) => {
+  try {
+    const { _id } = req.body;
+    const todo_find = await Todo_List.findOne({ _id });
+    if(!todo_find)
+    {
+      return res.status(404).send({ message: "Todo does not exist"});
+    }
+    todo_find
+      .deleteOne()
+      .then(() => res.status(200).json({ message: "Todo Deleted!" }));
+  } catch (err) {
+    res.status(400).send({ message: "Failed to delete todo: " + err.message });
   }
 });
 
